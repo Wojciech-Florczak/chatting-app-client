0399b0c22e8d2e96ca0e3363e3c3a3c5cd31e15f
wojciech-florczak <florczak@wojciech.dev>
2020-03-20 14:49:10 +0100

    Feat: update Join comp and create styles file for it to fill later

--- src/components/Join/index.js
+++ src/components/Join/index.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useStyles } from "./styles";
 
 export default function Join() {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const classes = useStyles();
+
   return (
-    <div>
-      Join
+    <div className={classes.outerWrapper}>
+      <div className={classes.innerWrapper}>
+        <h1 className={classes.heading}>Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className={classes.input}
+            type="text"
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className={classes.input}
+            type="text"
+            onChange={e => setRoom(e.target.value)}
+          />
+        </div>
+        <Link
+          to={`/chat?name=${name}&room=${room}`}
+          onClick={e => (!name || !room) && e.preventDefault()}
+        >
+          <button className={classes.button}>Sign In</button>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
---
+++ src/components/Join/styles.js
@@ -0,0 +1,9 @@
+import { createUseStyles } from "react-jss";
+
+export const useStyles = createUseStyles({
+  outerWrapper: {},
+  innerWrapper: {},
+  heading: {},
+  input: {},
+  button: {}
+});
